31abb808790afa6a654d5e9a3511bdd6fd1892e2
zjhinsist <2152889763@qq.com>
2022-11-21 15:56:22 +0800

    顶部加载

--- themes/butterfly/scripts/helpers/page.js
+++ themes/butterfly/scripts/helpers/page.js
@@ -95,3 +95,13 @@ hexo.extend.helper.register('findArchivesTitle', function (page, menu, date) {
 
   return loop(menu) || defaultTitle
 })
+
+// 最新文章
+hexo.extend.helper.register('newPost', function() {
+  let name, time;
+  hexo.locals.get('posts').map((item, index) => {
+      if (index == 0) name = item.title, time = item.date
+      else if (item.date > time) { name = item.title, time = item.date }
+  });
+  return name
+})
